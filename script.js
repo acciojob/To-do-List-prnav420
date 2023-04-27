47d85036920cdf56ad7aab93f71cab81115ef43e
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-04-27 19:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+var addTodoBtn = document.getElementById("addTodoBtn");
+
+var newTodoInput = document.getElementById("newTodoInput");
+var todoList = document.getElementById("todoList");
+
+function result() {
+  if (newTodoInput.value) {
+    var li = document.createElement("li");
+li.textContent = newTodoInput.value;
+    todoList.appendChild(li);
+ newTodoInput.value="";
+  }
+}
+addTodoBtn.addEventListener("click", result);
